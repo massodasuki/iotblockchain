5fa034c67de87f8d65ea1d8d6def7f0dc645809e
massodasuki <massolihen.dasuki@razer.com>
2021-03-14 00:34:33 +0800

    sdsdsdas

--- routes/benchmark.js
+++ routes/benchmark.js
@@ -63,64 +63,80 @@ conn.postTransactionCommit(txSigned)
 /* POST  */
 router.post('/', function(req, res, next) {
 
+
     const driver = require('bigchaindb-driver')
-    const base58 = require('bs58');
-    const crypto = require('crypto');
-    const { Ed25519Sha256 } = require('crypto-conditions');
 
-    // BigchainDB server instance (e.g. https://example.com/api/v1/)
-    const API_PATH = 'http://localhost:9984/api/v1/'
-
-    // Create a new keypair.
     const alice = new driver.Ed25519Keypair()
-
-    // Construct a transaction payload
+    const conn = new driver.Connection('http://localhost:9984/api/v1/')
     const tx = driver.Transaction.makeCreateTransaction(
-        // Define the asset to store, in this example it is the current temperature
-        // (in Celsius) for the city of Berlin.
-        // { city: 'Berlin, DE', temperature: 22, datetime: new Date().toString() },
-        { age: req.body.age,
-          sex: req.body.sex,
-          bmi: req.body.bmi,
-         smoker: req.body.smoker,
-         region: req.body.region,
-         charge: req.body.charge
-        }
-
-        // Metadata contains information about the transaction itself
-        // (can be `null` if not needed)
-        // { what: 'My first BigchainDB transaction' },
-
-        // A transaction needs an output
+        { message: 'Blockchain all the things!' },
+        null,
         [ driver.Transaction.makeOutput(
-                driver.Transaction.makeEd25519Condition(alice.publicKey))
-        ],
-        alice.publicKey
-    )
-
-    // Sign the transaction with private keys
+            driver.Transaction.makeEd25519Condition(alice.publicKey))],
+        alice.publicKey)
     const txSigned = driver.Transaction.signTransaction(tx, alice.privateKey)
-
-    // Or use delegateSignTransaction to provide your own signature function
-    // function signTransaction() {
-    //     // get privateKey from somewhere
-    //     const privateKeyBuffer = Buffer.from(base58.decode(alice.privateKey))
-    //     return function sign(transaction, input, transactionHash) {
-    //         const ed25519Fulfillment = new Ed25519Sha256();
-    //         ed25519Fulfillment.sign(
-    //             Buffer.from(transactionHash, 'hex'),
-    //             privateKeyBuffer
-    //         );
-    //         return ed25519Fulfillment.serializeUri();
-    //     };
-    // }
-    // const txSigned = driver.Transaction.delegateSignTransaction(tx, signTransaction())
-
-    // Send the transaction off to BigchainDB
-    const conn = new driver.Connection(API_PATH)
-    console.log(txSigned);
     conn.postTransactionCommit(txSigned)
-    .then(retrievedTx => console.log('Transaction', retrievedTx.id, 'successfully posted.'))
+
+
+
+    // const driver = require('bigchaindb-driver')
+    // const base58 = require('bs58');
+    // const crypto = require('crypto');
+    // const { Ed25519Sha256 } = require('crypto-conditions');
+
+    // // BigchainDB server instance (e.g. https://example.com/api/v1/)
+    // const API_PATH = 'http://localhost:9984/api/v1/'
+
+    // // Create a new keypair.
+    // const alice = new driver.Ed25519Keypair()
+
+    // // Construct a transaction payload
+    // const tx = driver.Transaction.makeCreateTransaction(
+    //     // Define the asset to store, in this example it is the current temperature
+    //     // (in Celsius) for the city of Berlin.
+    //     // { city: 'Berlin, DE', temperature: 22, datetime: new Date().toString() },
+    //     { age: req.body.age,
+    //       sex: req.body.sex,
+    //       bmi: req.body.bmi,
+    //      smoker: req.body.smoker,
+    //      region: req.body.region,
+    //      charge: req.body.charge
+    //     }
+
+    //     // Metadata contains information about the transaction itself
+    //     // (can be `null` if not needed)
+    //     // { what: 'My first BigchainDB transaction' },
+
+    //     // A transaction needs an output
+    //     [ driver.Transaction.makeOutput(
+    //             driver.Transaction.makeEd25519Condition(alice.publicKey))
+    //     ],
+    //     alice.publicKey
+    // )
+
+    // // Sign the transaction with private keys
+    // const txSigned = driver.Transaction.signTransaction(tx, alice.privateKey)
+
+    // // Or use delegateSignTransaction to provide your own signature function
+    // // function signTransaction() {
+    // //     // get privateKey from somewhere
+    // //     const privateKeyBuffer = Buffer.from(base58.decode(alice.privateKey))
+    // //     return function sign(transaction, input, transactionHash) {
+    // //         const ed25519Fulfillment = new Ed25519Sha256();
+    // //         ed25519Fulfillment.sign(
+    // //             Buffer.from(transactionHash, 'hex'),
+    // //             privateKeyBuffer
+    // //         );
+    // //         return ed25519Fulfillment.serializeUri();
+    // //     };
+    // // }
+    // // const txSigned = driver.Transaction.delegateSignTransaction(tx, signTransaction())
+
+    // // Send the transaction off to BigchainDB
+    // const conn = new driver.Connection(API_PATH)
+    // console.log(txSigned);
+    // conn.postTransactionCommit(txSigned)
+    // .then(retrievedTx => console.log('Transaction', retrievedTx.id, 'successfully posted.'))
 
     res.render('benchmark');
 });
